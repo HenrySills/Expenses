fa890e71555ff33fafe35c57a0a7cee2ae899577
Henry <hes899@mail.missouri.edu>
2019-08-23 16:15:11 -0500

    First Commit

---
+++ Expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Henry Sills on 8/23/19.
+//  Copyright © 2019 Henry Sills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
