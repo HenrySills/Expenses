d136c25a29262dca72f583b35ff95e0712f6cfa5
Henry <hes899@mail.missouri.edu>
2019-08-23 20:36:09 -0500

    Expense report finished

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by Henry Sills on 8/23/19.
+//  Copyright © 2019 Henry Sills. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
